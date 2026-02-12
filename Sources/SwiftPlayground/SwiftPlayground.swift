ec5d6b8f3c1f0675b4287f3d87fbb22aa95e66c4
archie-domaneschi-2024 <159369887+archieDomaneschi@users.noreply.github.com>
2026-02-12 22:11:43 +0000

    done

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,11 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        let mixed = ["cat", "7", "owl", "15", "dog", "3"]
+        let numbers = mixed.compactMap{Int($0)}
+        print(numbers)
+        let isNumber = numbers.allSatisfy {Int($0) != nil}  
+        print(isNumber)
+
     }
 }
