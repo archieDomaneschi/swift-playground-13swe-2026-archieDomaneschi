762cdf180cfeef79279bf1a4e1a07ec9baeb8275
archie-domaneschi-2024 <159369887+archieDomaneschi@users.noreply.github.com>
2026-02-16 01:40:27 +0000

    broken revising maps

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -7,8 +7,20 @@ struct SwiftPlayground {
         let mixed = ["cat", "7", "owl", "15", "dog", "3"]
         let numbers = mixed.compactMap{Int($0)}
         print(numbers)
-        let isNumber = numbers.allSatisfy {Int($0) != nil}  
+        let isNumber = mixed.allSatisfy {Int($0) != nil}  
         print(isNumber)
+        let sightings = [
+        (name: "moth", score: 3),
+        (name: "wolf", score: 9),
+        (name: "raven", score: 4),
+        (name: "mist", score: 7),
+        (name: "wisp", score: 2)
+        ]
+            let sightingsFilter = sightings.filter{ sighting in return sighting.name.hasPrefix("m")  
+            || sighting.name.hasPrefix("w")}
+            print(sightingsFilter)
+            let filterdScores = sightingsFilter.map({ ($0  in return score ) })
+
 
     }
 }
