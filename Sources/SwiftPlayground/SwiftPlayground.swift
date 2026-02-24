457ba2e6759a1c8c094cabc7241208e272592d3e
archie-domaneschi-2024 <159369887+archieDomaneschi@users.noreply.github.com>
2026-02-24 23:07:56 +0000

    finished

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,5 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+// book struct holds author title and length of book plus a ethod to print a book summary
 struct Book{
     var title: String
     var author: String
@@ -12,13 +14,13 @@ struct Book{
         """
     }
 }
-
+// open function to print a book summary without creating an instance 
 func summary(title: String, author: String, pages: Int) -> String {
     return """
     \(title) by \(author) has \(pages) pages.
     """
 }
-
+// contains 2 static functions that cnonvert farenheit into celsius or celsius into farenheit
 struct Temperature {
 
 
@@ -29,7 +31,7 @@ struct Temperature {
         return (fahrenheit - 32) * 5/9
     }
 }
-
+// timer struct contains 2 properteis and 3 methods to start the timer, tick the timer and reset the timer
 struct Timer{ 
     var seconds : Double
     var isRunning : Bool
@@ -47,7 +49,7 @@ struct Timer{
         isRunning = false
     }
 }
-
+// cart struct contains 2 properties and 3 methods to add an item to the cart, check if the cart qualifies for free shipping and to print a message about free shipping
 struct Cart{
     var itemsCount : Int
 
@@ -67,13 +69,13 @@ struct Cart{
         if itemsCount >= Cart.freeShippingThreshold {
             return "you are eligable for free shipping"
         } else {
-            return "You have \(itemsCount), you need \(Cart.freeShippingThreshold) to be eligable for free shipping."
+            return "You have \(itemsCount) items, you need \(Cart.freeShippingThreshold) to be eligable for free shipping."
         }
     
 
     }
 }
-
+// stuct badge contains 2 properties and a computed property to print the name and level of the badge in a specific format
 struct Badge{
     var name: String
     var level: Int
@@ -88,14 +90,18 @@ struct Badge{
 @main
 struct SwiftPlayground {
     static func main() {
+        // declears two constants using the struct to store information of two books 
         let book1 = Book(title: "The Great Gatsby", author: "F. Scott Fitzgerald", pages: 180)
         let book2 = Book(title: "To Kill a Mockingbird", author: "Harper Lee", pages: 281)
+        // Book task, prints out book summaries using open function
+        print(summary(title: book1.title , author: book1.author, pages: book1.pages))
+        print(summary(title: book2.title, author: book2.author, pages: book2.pages))
+        // prints out temperature conversions using the static functions in the Temperature struct passing in values for temp 
         print(Temperature.toFahrenheit(celsius: 22))
         print(Temperature.toCelsius(fahrenheit: 65))
         print(Temperature.toCelsius(fahrenheit: 120))
-        print(book1.bookSummary())
-        print(book2.bookSummary())
         
+        // timer task, creates an instance of the timer struct and calls the start, tick and reset functions to show how the seconds property changes with each function call
         var timer = Timer(seconds: 0, isRunning: false)
         timer.start()
         print(timer.seconds)
@@ -103,18 +109,19 @@ struct SwiftPlayground {
         print(timer.seconds)
         timer.reset()
         print(timer.seconds)
-        // all instance behaviour as function used are being called from the struct and I created an instance of the struct "Cart" when i created var cart1
+        // cart task, creates an instance of the cart struct and uses a while loop to add items to the cart and print out the shipping message after each item is added to show how the message changes as the items count increases
         var amountofcycles = 0 
         var cart1 = Cart(itemsCount: 0) 
         while amountofcycles < 6 {
             cart1.addItem()
             
             amountofcycles += 1
-            dump(cart1)
+            print(cart1)
             // data is stored in the cart1 vairable so we can call the shippingMessage function to check if we are eligable for free shipping
             print(cart1.shippingMessage())
 
         }
+        // badge task, creates a constant using the struct to stor information about a badge and then prints out the summary using the label method from the struct. 
         let badge1 = Badge(name: "sheriff", level: 3)
         print(badge1.label)
 
