c176a06bade4dc4eed807a19e1686bd8ab3826cf
archie-domaneschi-2024 <159369887+archieDomaneschi@users.noreply.github.com>
2026-02-26 22:11:27 +0000

    partially finished

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,9 +1,43 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Foundation
+struct Student: Identifiable{
+    var name: String
+    var age: Int
+    let id: UUID
+}
+struct Course: CustomStringConvertible{
+    let id: UUID
+    let title : String
 
+    var description: String{
+        return "Course: \(title) with id: \(id)"
+    }
+
+}
+
+
+struct Enrolment: Codable{
+    let studentID: Int
+    let CourseID: Int
+}
+
+struct ScoreEntry: Comparable {
+    let studentID:UUID
+    var points : Float
+    static func < (lhs: ScoreEntry, rhs: ScoreEntry) -> Bool {
+        return lhs.points < rhs.points
+    }
+
+}
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        let enrolment = Enrolment(studentID: 26712, CourseID: 68321)
+        let data = try! JSONEncoder().encode(enrolment)
+        print(data)
+        let decodedEnrolment = try! JSONDecoder().decode(Enrolment.self, from: data)
+        print(decodedEnrolment)
+
     }
 }
