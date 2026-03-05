1b42e1e9fa0905a111d0c666396bcc4dc09cca81
archie-domaneschi-2024 <159369887+archieDomaneschi@users.noreply.github.com>
2026-03-05 22:13:26 +0000

    done

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -21,14 +21,18 @@ struct ScoreEntry: Comparable {
         return lhs.points < rhs.points
     }
 }
-struct Enrolment: Hashable{
-    let studentID: Int
+struct EnrolmentCodeable: Codable{
+    let studentID: UUID
+    let CourseID: String
+}
+
+struct Enrolment: Hashable, Codable{
+    let studentID: UUID
     let CourseID: String
     func hash(into hasher: inout Hasher) {
         hasher.combine(studentID)
         hasher.combine(CourseID)
-    
-    }
+        
     
 }
 
@@ -36,9 +40,38 @@ struct Enrolment: Hashable{
 @main
 struct SwiftPlayground {
     static func main() {
-        let enrolment = Enrolment(studentID: 26712, CourseID: "SWE13")
+        let enrolment = Enrolment(studentID: UUID(), CourseID: "SWE13")
+        print(enrolment.hashValue)
+        let student1 = Student(name: "Jules", age: 16, id: UUID())
+        let student2 = Student(name: "Stan", age: 17, id: UUID())
+        let student3 = Student(name: "Ash", age: 18, id: UUID())
+        let course1 = Course(id: UUID(), title: "13SWE")
+        let enrolment1 = Enrolment(studentID: (student1.id), CourseID: (enrolment.CourseID))
+        let enrolment2 = Enrolment(studentID: (student2.id), CourseID: (enrolment.CourseID))
+        let enrolment3 = Enrolment(studentID: (student1.id), CourseID: (enrolment.CourseID))
+        print(course1)
 
-       
+        let enrolment1Encoded = try? JSONEncoder().encode(enrolment1)
+        let enrolment1Decoded = try? JSONDecoder().decode(Enrolment.self, from: enrolment1Encoded!)
+        let enrolment2Encoded = try? JSONEncoder().encode(enrolment2)
+        let enrolment2Decoded = try? JSONDecoder().decode(Enrolment.self, from: enrolment2Encoded!)
+        let allenrolmentsSet: Set<Enrolment> = [enrolment1, enrolment2, enrolment3]
+        print(allenrolmentsSet.count)
+        print("enrolment 1 hashvalue : \(enrolment1Decoded.hashValue)")
+        print("enrolment 2 hashvalue : \(enrolment2Decoded.hashValue)")
+
+        let student1Score = ScoreEntry(studentID: student1.id, points: 50.0)
+        let student2Score = ScoreEntry(studentID: student2.id, points: 55.0)
+        let student3Score = ScoreEntry(studentID: student3.id, points: 55.0)
+        let scoresArray = [student1Score.points, student2Score.points, student3Score.points]
+        let scoresArraySorted = scoresArray.sorted()
+        print("Sorted scores: \(scoresArraySorted)")
+        if student2Score.points == student3Score.points {
+            print("Student 2 and Student 3 have the same score.")
+        } else {
+            print("Student 2 and Student 3 have different scores.")
+        }
+
         
         
 
