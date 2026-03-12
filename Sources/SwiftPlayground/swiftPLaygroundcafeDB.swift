9e779674a04275cf87b5662f0a609c98a75d813c
archie-domaneschi-2024 <159369887+archieDomaneschi@users.noreply.github.com>
2026-03-12 22:11:40 +0000

    halfway done

--- Sources/SwiftPlayground/swiftPLaygroundcafeDB.swift
+++ Sources/SwiftPlayground/swiftPLaygroundcafeDB.swift
@@ -1,11 +1,102 @@
 import Foundation
 import GRDB
+///a purchaser is the name for the reservation or purchaser at the vafe
+
+struct Purchaser: Identifiable, Codable, FetchableRecord, PersistableRecord{
+    /// an id given to each purchaser
+    let id: Int
+
+    ///name of the Purchasser
+    var name: String
+
+    /// amount of people at a table
+    var count: Int
+
+    /// name of the table 
+    var reservedTable: String
+
+    enum CodingKeys: String, CodingKey {
+        case id = "purchaserID"
+        case name = "Name"
+        case count = "Count"
+        case reservedTable = "reservedTable"
+    }
+}
+/// contents of the order that is sent to the kitchen 
+struct orderLine:Codable, FetchableRecord, PersistableRecord{
+    /// an id given to each purchaser
+    let id: Int
+
+    ///name of the Purchasser
+    var quantity: Int
+
+    /// amount of people at a table
+    let itemID: Int
+
+
+    enum CodingKeys: String, CodingKey {
+        case id = "OrdrID"
+        case quantity = "Quantity"
+        case itemID = "ItemID"
+    }
+}
+
+struct Order: Identifiable, Codable, FetchableRecord, PersistableRecord{
+    /// an id given to each order
+    let id: Int
+
+    ///namount of an item
+    var amount: Int
+
+    /// purchaser ID to reference purchaser table
+    let purchaserID: Int
+
+    enum CodingKeys: String, CodingKey {
+        case id = "OrderID"
+        case amount = "Amount"
+        case purchaserID = "PurchaserID"
+    }
+}
+struct Item: Identifiable, Codable, FetchableRecord, PersistableRecord{
+    /// an id given to each item
+    let id: Int
+
+    ///name of the item
+    var name: String
+
+    /// amount of items
+    var amount: Float
+
+
+
+    enum CodingKeys: String, CodingKey {
+        case id = "ItemID"
+        case name = "Name"
+        case amount = "Amount"
+        
+    }
+}
+
+
 
 @main
 struct SwiftPlayground{
     static func main(){
         let dpath = "Sources/SwiftPlayground/cafe.db"
-        
+        do{
+
+            let dbqeue = try DatabaseQueue(path: dpath)
+            print("connected to database")
+            /// makes sure we are connected corrrectly
+            try dbqeue.read({database in 
+            try database.dumpSchema()})
+            try Purchaser.filter( key: 
+            "ReservedTable")
+            
+        } catch {print(error)}
+
+        }
+
 
 
 
@@ -15,4 +106,4 @@ struct SwiftPlayground{
 
 
     }
-}
+
