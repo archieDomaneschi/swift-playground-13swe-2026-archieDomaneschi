545dab5ec2101eabe5b5001aae12b450b94a1307
archie-domaneschi-2024 <159369887+archieDomaneschi@users.noreply.github.com>
2026-04-22 00:33:36 +0000

    save

--- Package.swift
+++ Package.swift
@@ -5,12 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftPlayground",
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "SwiftPlayground"
-        ),
+    dependencies: [
+        .package(url: "https://github.com/groue/GRDB.swift", exact: "7.10.0")
     ],
- 
-
+    targets: [
+        .executableTarget(
+            name: "SwiftPlayground",
+            dependencies: [
+                .product(name: "GRDB", package: "grdb.swift")
+            ]
+        )
+    ]
+)
