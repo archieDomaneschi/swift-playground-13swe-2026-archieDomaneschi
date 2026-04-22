c8a8e631af2f5c7125f86883b0a381a3684114b3
archie-domaneschi-2024 <159369887+archieDomaneschi@users.noreply.github.com>
2026-04-22 00:36:44 +0000

    cant make db

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,9 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import GRDB
+
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        let dbPath = "./library.db"
+        guard let dbQueue = try? DatabaseQueue(path: dbPath) else {
+            fatalError("Could not open database.")
+        }
     }
 }
