efc35b644792e0071eba24b4e87c87936543ddd1
archie-domaneschi-2024 <159369887+archieDomaneschi@users.noreply.github.com>
2026-04-23 21:25:08 +0000

    nned to start coding now

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -6,9 +6,19 @@ import GRDB
 @main
 struct SwiftPlayground {
     static func main() {
-        let dbPath = "./library.db"
+        let dbPath = "Sources/SwiftPlayground/library.db"
         guard let dbQueue = try? DatabaseQueue(path: dbPath) else {
-            fatalError("Could not open database.")
+            print("Could not open database.")
+            return
         }
+        print("Connected to database.")
+
+        //change to input later, placeholder rn 
+    /// function to fetch all records from a table and print them
+    
+        let tables = ["Books","Loan,","Customer"]
+
+
+    /// function to search for a specfic record
     }
 }
